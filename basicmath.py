38f4395f62bff610109cedbe97f12d8794af8576
Liresol <bugme1210@yahoo.com>
2017-07-08 21:09:28 -0400

    Added stats display
    Added a window to display the stats and touched up some of the other
    text.

--- basicmath.py
+++ basicmath.py
@@ -22,7 +22,7 @@ def addition_problem():
     add2 = random.randint(min, max)
     question = str(add1) + " + " + str(add2) + " = "
     answer = add1 + add2
-    return problem.Problem(question,answer, 'addition')
+    return problem.Problem(question,answer, 'Addition')
 """
 Creates a random subtraction problem using numbers from min to max, inclusive
 """
@@ -37,7 +37,7 @@ def subtraction_problem():
         sub1 = sub2 + sub1
     question = str(sub1) + " - " + str(sub2) + " = "
     answer = sub1 - sub2
-    return problem.Problem(question, answer, 'subtraction')
+    return problem.Problem(question, answer, 'Subtraction')
         
 def multiplication_problem(min = 10, max = 100):
     min = int(config['Multiplication']['min'])
@@ -46,7 +46,7 @@ def multiplication_problem(min = 10, max = 100):
     mul2 = random.randint(min, max)
     question = str(mul1) + " · " + str(mul2) + " = "
     answer = mul1 * mul2
-    return problem.Problem(question, answer, 'multiplication')
+    return problem.Problem(question, answer, 'Multiplication')
 def integer_division_problem(min = 10, max = 100):
     min = int(config['Division']['min'])
     max = int(config['Division']['max'])
@@ -54,4 +54,4 @@ def integer_division_problem(min = 10, max = 100):
     div2 = random.randint(min, max)
     question = str(div1*div2) + " / " + str(div1) + " = "
     answer = div2
-    return problem.Problem(question, answer, 'int_division')
+    return problem.Problem(question, answer, 'Integer Division')
--- stats.py
+++ stats.py
@@ -29,6 +29,8 @@ def get_last_n_days(n):
         day -= delta
     return dlist
 
+
+
 """
 A class for keeping track of the stats of a single session.
 Stats are stored as a dict with the question type as the key
@@ -65,20 +67,33 @@ class Stats:
                     self.totals[k][i] += s.totals[k][i]
             else:
                 self.totals[k] = s.totals[k]
-    def get_avg_times(self):
+    def get_display_vals(self):
         avgs = {}
         for k in self.totals:
             lst = []
-            lst.append(self.totals[k][0]/self.totals[k][1])
+            if self.totals[k][1] is not 0:
+                lst.append(self.totals[k][0]/self.totals[k][1])
+                #WARNING: using inf may create problems
+            else:
+                lst.append(float('inf'))
             if self.totals[k][3] is not 0:
                 lst.append(self.totals[k][2]/self.totals[k][3])
-                #WARNING: MESSING WITH INFINITY
+                #WARNING: using inf may create problems
             else:
                 lst.append(float('inf'))
             avgs[k] = lst
+            if self.totals[k][1] is not 0:
+                lst.append(self.totals[k][3]/self.totals[k][1])
+            else:
+                lst.append(0)
         return avgs
 
 path = 'stats.json'
+"""
+Records the stats of a session into a JSON file
+Since there is only one set of stats at any time (for now),
+everything is a static method
+"""
 class StatsWriter:
     """
     Returns a stats class representing today's stats.
@@ -90,27 +105,23 @@ class StatsWriter:
             with open(path) as data_file:
                 data = json.load(data_file)
             ret.set_dict(data[date])
-        except KeyError:
+        except (KeyError, TypeError) as e:
             print("No sessions done today")
         return ret
     def get_n_day_stats(n):
         ret = Stats()
-        with open(path) as data_file:
-            data = json.load(data_file)
+        try:
+            with open(path, "r") as infile:
+                data = json.load(infile)
+        except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
+            print("No history data")
+            data = {}
         for i in get_last_n_days(n):
             try:
                 ret.merge_stats(Stats(data[i]))
-            except KeyError:
+            except (KeyError, TypeError) as e:
                 pass
         return ret
-    def get_week_stats():
-        return get_n_day_stats(7)
-    def get_30_day_stats():
-        return get_n_day_stats(30)
-    def get_90_day_stats():
-        return get_n_day_stats(90)
-    def get_year_stats():
-        return get_n_day_stats(365)
     def write_session(stats):
         date = get_date()
         try:
@@ -124,10 +135,119 @@ class StatsWriter:
             day_stats.set_dict(data[date])
         day_stats.merge_stats(stats)
         data[date] = day_stats.get_dict()
-        print(data)
         with open(path, "w") as outfile:
             json.dump(data, outfile)
+    def delete_stats():
+        with open(path, "w") as outfile:
+            json.dump({}, outfile)
+class StatsWindow:
+    def __init__(self, master):
+        self.master = master
+        self.window = Toplevel(master)
+        self.frame = ttk.Frame(self.window)
+        self.window.title('Stats')
+        self.dstats = StatsWriter.get_today_stats()
+        self.wstats = StatsWriter.get_n_day_stats(7)
+        self.stats_30 = StatsWriter.get_n_day_stats(30)
+        self.stats_90 = StatsWriter.get_n_day_stats(90)
+        self.stats_365 = StatsWriter.get_n_day_stats(365)
 
-class StatsDisplay:
-    def __init__(self):
+        self.timeslot = StringVar()
+        self.selections = ttk.Combobox(self.window, textvariable = self.timeslot)
+        self.selections.bind('<<ComboboxSelected>>', self.parse_display)
+        self.selections['values'] = ("Today's Stats","Week's Stats","30 Day Stats","90 Day Stats","365 Day Stats")
+        self.selections.grid()
+        self.frame.grid()
+        self.day_display()
+    def parse_display(self, *args):
+        self.clean_display()
+        text = self.timeslot.get()
+        if text == "Today's Stats":
+            self.day_display()
+        elif text == "Week's Stats":
+            self.week_display()
+        elif text == "30 Day Stats":
+            self.thirty_display()
+        elif text == "90 Day Stats":
+            self.ninty_display()
+        elif text == "365 Day Stats":
+            self.year_display()
+    def day_display(self, *args):
+        r = 0
+        avgs = self.dstats.get_display_vals()
+        self.frame.grid()
+        for k in avgs:
+            label1 = ttk.Label(self.frame, text=k + " ", font="Helvetica 15 bold")
+            label2 = ttk.Label(self.frame, text="Average time per answer: " + str(avgs[k][0]) + 's')
+            label3 = ttk.Label(self.frame, text="Average time per correct answer: " + str(avgs[k][1])+ 's')
+            label4 = ttk.Label(self.frame, text="Accuracy: " + str(100* avgs[k][2]) + "%")
+            label1.grid(row = r)
+            label2.grid(row = r+1)
+            label3.grid(row = r+2)
+            label4.grid(row = r+3)
+            r += 4
+    def week_display(self, *args):
+
+        r = 0
+        avgs = self.wstats.get_display_vals()
+        self.frame.grid()
+        for k in avgs:
+            label1 = ttk.Label(self.frame, text=k + " ", font="Helvetica 15 bold")
+            label2 = ttk.Label(self.frame, text="Average time per answer: " + str(avgs[k][0]) + 's')
+            label3 = ttk.Label(self.frame, text="Average time per correct answer: " + str(avgs[k][1])+ 's')
+            label4 = ttk.Label(self.frame, text="Accuracy: " + str(100* avgs[k][2]) + "%")
+            label1.grid(row = r)
+            label2.grid(row = r+1)
+            label3.grid(row = r+2)
+            label4.grid(row = r+3)
+            r += 4
+    def thirty_display(self, *args):
+        r = 0
+        avgs = self.stats_30.get_display_vals()
+        self.frame.grid()
+        for k in avgs:
+            label1 = ttk.Label(self.frame, text=k + " ", font="Helvetica 15 bold")
+            label2 = ttk.Label(self.frame, text="Average time per answer: " + str(avgs[k][0]) + 's')
+            label3 = ttk.Label(self.frame, text="Average time per correct answer: " + str(avgs[k][1])+ 's')
+            label4 = ttk.Label(self.frame, text="Accuracy: " + str(100* avgs[k][2]) + "%")
+            label1.grid(row = r)
+            label2.grid(row = r+1)
+            label3.grid(row = r+2)
+            label4.grid(row = r+3)
+            r += 4
+
         pass
+    def ninty_display(self, *args):
+        r = 0
+        avgs = self.stats_90.get_display_vals()
+        self.frame.grid()
+        for k in avgs:
+            label1 = ttk.Label(self.frame, text=k + " ", font="Helvetica 15 bold")
+            label2 = ttk.Label(self.frame, text="Average time per answer: " + str(avgs[k][0]) + 's')
+            label3 = ttk.Label(self.frame, text="Average time per correct answer: " + str(avgs[k][1])+ 's')
+            label4 = ttk.Label(self.frame, text="Accuracy: " + str(100* avgs[k][2]) + "%")
+            label1.grid(row = r)
+            label2.grid(row = r+1)
+            label3.grid(row = r+2)
+            label4.grid(row = r+3)
+            r += 4
+
+        pass
+    def year_display(self, *args):
+        r = 0
+        avgs = self.stats_365.get_display_vals()
+        self.frame.grid()
+        for k in avgs:
+            label1 = ttk.Label(self.frame, text=k + " ", font="Helvetica 15 bold")
+            label2 = ttk.Label(self.frame, text="Average time per answer: " + str(avgs[k][0]) + 's')
+            label3 = ttk.Label(self.frame, text="Average time per correct answer: " + str(avgs[k][1])+ 's')
+            label4 = ttk.Label(self.frame, text="Accuracy: " + str(100* avgs[k][2]) + "%")
+            label1.grid(row = r)
+            label2.grid(row = r+1)
+            label3.grid(row = r+2)
+            label4.grid(row = r+3)
+            r += 4
+    def clean_display(self):
+        for child in self.frame.winfo_children():
+            child.destroy()
+
